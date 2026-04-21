c5d2780a719cd117f790e9dfccab5a67240ccad3
Haseeb Raza <haseeb.javed715@gmail.com>
2026-04-21 17:33:50 +0200

    Export consolidation schema module

--- src/llm_survey/schemas/__init__.py
+++ src/llm_survey/schemas/__init__.py
@@ -1,5 +1,13 @@
 from llm_survey.schemas.clarification import ClarificationPlan
+from llm_survey.schemas.consolidation import ConsolidatedModel, ConflictReport, LiteratureValidationReport
 from llm_survey.schemas.extraction import ChunkExtractionResult
 from llm_survey.schemas.gap import CrossChunkGapReport
 
-__all__ = ["ChunkExtractionResult", "CrossChunkGapReport", "ClarificationPlan"]
+__all__ = [
+    "ChunkExtractionResult",
+    "ClarificationPlan",
+    "ConsolidatedModel",
+    "ConflictReport",
+    "CrossChunkGapReport",
+    "LiteratureValidationReport",
+]
